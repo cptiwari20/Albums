25997940536e3e4a30239dd0894c5a6e2d7c949d
cptiwari20 <cptiwari20@gmail.com>
2018-12-28 11:43:45 +0530

    react-native setup

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Header extends Component {
+  render(){
+    return (
+      <Text>Albums-Project</Text>
+    )
+  }
+};
+
+export default Header;
